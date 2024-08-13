08c29560bc9f98140f7e4a65578f17d88b4b5d94
yui1987 <140272762+yui1987@users.noreply.github.com>
2024-08-13 11:16:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # Modify default IP (修改默认IP)
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme(修改默认主题)
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname（修改主机名）
-sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
